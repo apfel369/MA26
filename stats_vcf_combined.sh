5809c03f69f0a2a41368e9d8deec00bf44478c40
apfel369 <sabine2003.sr@gmail.com>
2025-11-08 13:25:12 +0100

    test2

--- stats_vcf_combined.sh
+++ stats_vcf_combined.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 BCF_DIR="/home/Drives/HDD04_06T_SDF/Buckfast_08.2024/AmelHAv3.1/bcftool_vcf_outputv2/"
-OUTFILE="/home/Drives/HDD04_06T_SDF/Buckfast_08.2024/AmelHAv3.1/bcftool_vcf_outputv2/all_stats_combined.txt"
-
-echo "# bcftools stats results for all samples" > "$OUTFILE"
+OUTFILE="/home/Drives/HDD04_06T_SDF/Buckfast_
 
 for f in ${BCF_DIR}/*.bcf; do
   base=$(basename "$f" .bcf)
   echo "" >> "$OUTFILE"
   echo "=== Sample: $base ===" >> "$OUTFILE"
-  bcftools stats "$f" >> "$OUTFILE"
-done
-
+  bcftools s
 
 
 
